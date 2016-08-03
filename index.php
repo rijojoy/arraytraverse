d4818eef784a7a676e9172f7927c94667ff92bfd
RIJO JOY <rijojoy@gmail.com>
2016-08-03 08:02:00 +0530

    Revert "Revert "Index Changed""
    This reverts commit 7b7032996cfb33b18fa63ea168ed1d675c2c9a2c.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $data = $obj->loadData('data.json');
 $dataToArray = json_decode(json_encode($data),true);
 $result = $obj->loopRecursive($dataToArray);
 $returnResult = array_filter($result);
+/** Print the data **/
 echo "<pre>";
 print_r($returnResult);
 ?>
